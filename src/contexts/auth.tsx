cb918b5a902057ab789c19bb937b8a196d4681ab
lucasmarcell <lmarcell18@gmail.com>
2021-10-20 23:31:17 -0300

    first commit

---
+++ src/contexts/auth.tsx
@@ -0,0 +1,90 @@
+import { createContext, ReactNode, useEffect, useState } from "react";
+import { api } from "../services/api";
+
+type AuthResponse = {
+  token: string;
+  user: {
+    id: string;
+    avatar_url: string;
+    name: string;
+    login: string;
+  }
+}
+
+type User = {
+  id: string;
+  name: string;
+  login: string;
+  avatar_url: string;
+}
+
+type AuthContextData = {
+  user: User | null;
+  signInUrl: string;
+  singOut: () => void;
+}
+
+export const AuthContext = createContext({} as AuthContextData)
+
+type AuthProvider = {
+  children: ReactNode;
+}
+
+export function AuthProvider(props: AuthProvider) {
+  const [user, setUser] = useState<User | null>(null)
+
+
+  const signInUrl = `https://github.com/login/oauth/authorize?scope=user&client_id=8cf89ec29093b76917e9`;
+
+
+  async function signIn(githubCode: string) {
+    const response = await api.post<AuthResponse>('authenticate', {
+      code: githubCode,
+    })
+
+    const { token, user } = response.data;
+
+    localStorage.setItem('@dowhile:token', token);
+
+    api.defaults.headers.common.authorization = `Bearer ${token}`;
+
+    setUser(user);
+  }
+
+  function singOut() {
+    setUser(null)
+    localStorage.removeItem('@dowhile:token')
+  }
+
+  useEffect(() => {
+    const token = localStorage.getItem('@dowhile:token')
+
+    if (token) {
+      api.defaults.headers.common.authorization = `Bearer ${token}`;
+
+      api.get<User>('profile').then(response => {
+        setUser(response.data);
+      })
+    }
+  }, [])
+
+  useEffect(() => {
+    const url = window.location.href;
+    const hasGithubCode = url.includes('?code=');
+
+    if (hasGithubCode) {
+      const [urlWithoutCode, githubCode] = url.split('?code=')
+
+      window.history.pushState({}, '', urlWithoutCode)
+
+      signIn(githubCode)
+    }
+  }, [])
+
+
+  return (
+    <AuthContext.Provider value={{ signInUrl, user, singOut }} >
+      {props.children}
+    </AuthContext.Provider>
+  )
+}
